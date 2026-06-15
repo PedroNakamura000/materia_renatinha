2063da63244f4f2e95ffb16ec1eb2849dd062947
Pedro Nakamura <pegn2007@gmail.com>
2026-06-15 19:44:53 -0300

    Atividades e pastas alteradas

---
+++ Atividade_6/lista_numeros.js
@@ -0,0 +1,6 @@
+
+let numeros = [10, 20, 30, 40, 50];
+
+let soma = numeros[0] + numeros[numeros.length - 1];
+
+console.log("A soma do primeiro e do último elemento é:", soma);
---
+++ Atividade_6/livro.js
@@ -0,0 +1,9 @@
+let Livro = {
+    titulo: "Dom Casmurro",
+    autor: "Machado de Assis",
+    ano_publicacao: 1899
+};
+
+console.log(
+    `O livro "${Livro.titulo}" foi escrito por ${Livro.autor} e publicado em ${Livro.ano_publicacao}.`
+);
