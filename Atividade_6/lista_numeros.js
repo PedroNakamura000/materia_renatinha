3e533b3f6c90fff87c1e23ebf9c402b013ca46a7
Pedro Nakamura <pegn2007@gmail.com>
2026-06-18 14:58:56 -0300

    alterações denovo

--- Atividade_6/lista_numeros.js
+++
@@ -1,6 +0,0 @@
-
-let numeros = [10, 20, 30, 40, 50];
-
-let soma = numeros[0] + numeros[numeros.length - 1];
-
-console.log("A soma do primeiro e do último elemento é:", soma);
--- Atividade_6/livro.js
+++
@@ -1,9 +0,0 @@
-let Livro = {
-    titulo: "Dom Casmurro",
-    autor: "Machado de Assis",
-    ano_publicacao: 1899
-};
-
-console.log(
-    `O livro "${Livro.titulo}" foi escrito por ${Livro.autor} e publicado em ${Livro.ano_publicacao}.`
-);
